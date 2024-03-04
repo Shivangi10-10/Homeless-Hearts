c60a61a548323422580c79c253cc864e79093ad6
Ayushi Tripathi <140743838+ayu15-17@users.noreply.github.com>
2024-03-04 21:10:05 +0530

    Add files via upload

---
+++ src/pages/LoginForm.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import './LoginForm.css';
+
+const LoginForm = () => {
+  const navigate = useNavigate(); // Initialize the useNavigate hook
+  const [form, setForm] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+
+    navigate('/VolunteerDashboard');
+  };
+
+  return (
+    <div className='amatic-sc-regular'>
+    <div className="form-container">
+      <div className="image-div">
+        <img src="https://cdn.dribbble.com/users/1730991/screenshots/9889980/media/e1012f983af16ed28e2254591135feb1.gif" alt="Background" />
+      </div>
+      <div className="login-form-container">
+        <h2 className="form-title">Volunteer Login Form</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" id="username" value={form.username} onChange={handleChange} required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" value={form.password} onChange={handleChange} required />
+          </div>
+          <button type="submit" style={{fontFamily: 'amatic sc'}}>Login</button>
+        </form>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ src/pages/VolunteerForm.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import './VolunteerForm.css';
+import LoginForm from './LoginForm';
+
+const VolunteerForm = () => {
+  const navigate = useNavigate(); // Initialize useNavigate
+  const [form, setForm] = useState({
+    name: '',
+    phoneNumber: '',
+    email: '',
+    availability: [],
+    jobPreferences: [],
+  });
+
+  const handleChange = (e) => {
+    const { name, value, type } = e.target;
+    if (type === 'checkbox') {
+      setForm({
+        ...form,
+        [name]: [...form[name], value],
+      });
+    } else {
+      setForm({
+        ...form,
+        [name]: value,
+      });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+    navigate('/LoginForm');
+  };
+
+  return (
+    <div className="volunteer-form-container">
+      <h2 className="form-title">Volunteer Registration Form</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" id="name" value={form.name} onChange={handleChange} pattern="[A-Za-z\s]+" title="Only alphabets are allowed" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input type="text" name="username" id="username" value={form.username} onChange={handleChange} pattern="[A-Za-z\s]+" title="Only alphabets are allowed" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" value={form.password} onChange={handleChange} required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="phoneNumber">Phone Number</label>
+          <input type="tel" name="phoneNumber" id="phoneNumber" value={form.phoneNumber} onChange={handleChange} pattern="^\d{10}$" title="Enter a valid  10 digit phone number" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" value={form.email} onChange={handleChange} />
+        </div>
+        <div className="form-group">
+          <label>Availability</label>
+          <div className="checkbox-group">
+            <input type="checkbox" name="availability" value="morning" onChange={handleChange} /> Morning
+            <input type="checkbox" name="availability" value="afternoon" onChange={handleChange} /> Afternoon
+            <input type="checkbox" name="availability" value="evening" onChange={handleChange} /> Evening
+          </div>
+        </div>
+        <div className="form-group">
+          <label>Job Preferences</label>
+          <div className="checkbox-group">
+            <input type="checkbox" name="jobPreferences" value="collectingFood" onChange={handleChange} /> Collecting Food
+            <input type="checkbox" name="jobPreferences" value="distributingFood" onChange={handleChange} /> Distributing Food
+            <input type="checkbox" name="jobPreferences" value="vetVisit" onChange={handleChange} /> Vet Visit
+          </div>
+        </div>
+        <button type="submit">Apply</button>
+      </form>
+    </div>
+  );
+};
+
+export default VolunteerForm;
