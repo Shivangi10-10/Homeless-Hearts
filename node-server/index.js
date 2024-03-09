bdcd1f18f57a6a71c642b8c88d974dd3b709f436
palak222003 <140996045+palak222003@users.noreply.github.com>
2024-03-09 19:26:43 +0530

    Update index.js

--- node-server/index.js
+++ node-server/index.js
@@ -1,80 +1,37 @@
-const express = require('express');
-const cors=require('cors');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const VolunteerModel = require('./models/Volunteer')
 
-async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/form');
-    console.log('mongodb connected');
-  
-    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-  }
+const app = express()
+app.use(express.json())
+app.use(cors())
 
-  const volunteerSchema = new mongoose.Schema({
-    name: {
-       type: String,
-       required: true,
-       trim: true
-    },
-    phoneNumber: {
-       type: String,
-       required: true,
-       trim: true
-    },
-    email: {
-       type: String,
-       required: true,
-       trim: true,
-       unique: true,
-       match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
-    },
-    availability: {
-       type: [String],
-       required: true
-    },
-    jobPreferences: {
-       type: [String],
-       required: true
-    }
-   });
+mongoose.connect("mongodb://localhost:27017/Volunteer")
 
-   const volunteer = mongoose.model('volunteer', volunteerSchema);
+app.post('/login',(req,res) =>{
+    const {email,password} = req.body;
+    VolunteerModel.findOne({email:email})
+    .then(user =>{
+       if(user){
+        if(user.password === password){
+            res.json("Success")
+        }else{
+            res.json("the password is incorrect")
+        }
+       } else {
+        res.json("No record existed")
+       }
+    })
+ 
+ })
 
+app.post('/Volunteer',(req,res) =>{
+   VolunteerModel.create(req.body)
+   .then(employee => res.json(volunteers))
+   .catch(err => res.json(err))
+})
 
-const server=express();
-const corsOptions = {
-    origin: 'http://localhost:3000', // Adjust this to match your React app's origin
-    optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
-   };
-   
-
-server.use(cors(corsOptions));
-server.use(bodyParser.json());
-main().catch(err => console.log(err));
-
-
-
-server.post('/form', async (req, res) => {
-    console.log('Received POST request');
-    try {
-       let newVolunteer = new volunteer({
-         name: req.body.name,
-         phoneNumber: req.body.phoneNumber,
-         email: req.body.email,
-         availability: req.body.availability,
-         jobPreferences: req.body.jobPreferences,
-       });
-       const doc = await newVolunteer.save();
-       console.log(doc);
-       res.json(doc);
-    } catch (error) {
-       console.error('Error handling POST request:', error);
-       res.status(500).json({ message: 'An error occurred', error });
-    }
-});
-
-
-   
-server.listen(8080,()=>{
-    console.log('listening on port');
+app.listen(3001, () => {
+    console.log("server is running.")
 })
